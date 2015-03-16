ba61f3f5e4f123c92720fe338f9a72ee2e6abff0
BenjaminHarding <benjamin.p.harding@gmail.com>
2015-03-16 14:02:47 +1300

    Menu example acceptable now :) #6

--- examples/hud/menu.js
+++ examples/hud/menu.js
@@ -2,21 +2,41 @@ var state = new Kiwi.State('Play');
 
 state.create = function () {
 
+    this.menuWidth = 100;
+
     // Adds a menu widget to the defaultHUD of the game.
-    this.myButton1 = new Kiwi.HUD.Widget.MenuItem( this.game, 'Move Menu Left', 50, 50 );
-    this.myButton1.style.color = 'blue';
-    this.myButton1.style.backgroundColor = 'red';
+    this.myButton1 = new Kiwi.HUD.Widget.MenuItem( this.game, 'Move Left', -this.menuWidth, 0 );
+    this.myButton1.style.color = 'white';
+    this.myButton1.style.display = 'block';
+    this.myButton1.style.boxSizing = 'border-box';
+    this.myButton1.style.width = (this.menuWidth * 2).toString() + 'px';
+    this.myButton1.style.textAlign = 'center';
+    this.myButton1.style.cursor = 'pointer';
+    this.myButton1.style.padding = '0.5em 1em';
+    this.myButton1.style.backgroundColor = '#9c0';
 
-    this.myButton2 = new Kiwi.HUD.Widget.MenuItem( this.game, 'Move Menu Right', 50, 150 );
-    this.myButton2.style.color = 'green';
-    this.myButton2.style.backgroundColor = 'pink';
+    this.myButton2 = new Kiwi.HUD.Widget.MenuItem( this.game, 'Move Right', -this.menuWidth, 50 );
+    this.myButton2.style.color = 'white';
+    this.myButton2.style.display = 'block';
+    this.myButton2.style.boxSizing = 'border-box';
+    this.myButton2.style.width = (this.menuWidth * 2).toString() + 'px';
+    this.myButton2.style.textAlign = 'center';
+    this.myButton2.style.cursor = 'pointer';
+    this.myButton2.style.padding = '0.5em 1em';
+    this.myButton2.style.backgroundColor = '#c09';
 
-    this.myButton3 = new Kiwi.HUD.Widget.MenuItem( this.game, 'Move Menu To Center', 50, 250 );
-    this.myButton3.style.color = 'orange';
-    this.myButton3.style.backgroundColor = 'yellow';
+    this.myButton3 = new Kiwi.HUD.Widget.MenuItem( this.game, 'Center', -this.menuWidth, 100 );
+    this.myButton3.style.color = 'white';
+    this.myButton3.style.display = 'block';
+    this.myButton3.style.boxSizing = 'border-box';
+    this.myButton3.style.width = (this.menuWidth * 2).toString() + 'px';
+    this.myButton3.style.textAlign = 'center';
+    this.myButton3.style.cursor = 'pointer';
+    this.myButton3.style.padding = '0.5em 1em';
+    this.myButton3.style.backgroundColor = '#09c';
 
 
-    this.menu = new Kiwi.HUD.Widget.Menu( this.game, 100, 100 );
+    this.menu = new Kiwi.HUD.Widget.Menu( this.game, 0, 150 );
     this.menu.addMenuItem( this.myButton1 );
     this.menu.addMenuItem( this.myButton2 );
     this.menu.addMenuItem( this.myButton3 );
@@ -25,6 +45,8 @@ state.create = function () {
     this.menu.getMenuItem(0).input.onDown.add( this.leftButton, this );
     this.menu.getMenuItem(1).input.onDown.add( this.rightButton, this );
     this.menu.getMenuItem(2).input.onDown.add( this.resetButton, this );
+
+    this.resetButton();
 
 }
 
@@ -37,7 +59,7 @@ state.rightButton = function () {
 }
 
 state.resetButton = function () {
-    this.menu.x = this.game.stage.width / 2 - 75;
+    this.menu.x = this.game.stage.width / 2;
 }
 
 
